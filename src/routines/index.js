fa60e233179c1d291bf7eab658faf1a6efa04caf
Luigi Minardi <luigiminardim@gmail.com>
2020-08-01 01:00:52 -0300

    Update the routines.

--- src/routines/index.js
+++ src/routines/index.js
@@ -1,3 +1,14 @@
-import React from 'react'
 import AcidentesPorAnimaisPeconhentos from './AcidentesPorAnimaisPeconhentos'
-export default [AcidentesPorAnimaisPeconhentos]
+import Varicela from './Varicela'
+import FebreReumatica from './FebreReumatica'
+import AlergiaAProteinaDoLeiteDeVaca from './AlergiaAProteinaDoLeiteDeVaca'
+import ConstipacaoInstetinal from './ConstipacaoIntestinal'
+
+
+export default [
+  AcidentesPorAnimaisPeconhentos,
+  Varicela,
+  FebreReumatica,
+  AlergiaAProteinaDoLeiteDeVaca,
+  ConstipacaoInstetinal,
+]
