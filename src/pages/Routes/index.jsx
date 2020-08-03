09981fea9107de21706e0d766e696fd5aebc0647
Luigi Minardi <luigiminardim@gmail.com>
2020-08-03 13:21:44 -0300

    Start styling header.

--- src/pages/Routes/index.jsx
+++ src/pages/Routes/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, HeaderBackground } from '@react-navigation/stack';
 import Home from '../Home'
 import routines from '../../routines'
+import collorPallete from '../../assets/collorPallete'
 
 const Stack = createStackNavigator()
 
@@ -10,11 +11,22 @@ export default class Routes extends React.Component {
 
   render = () =>
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        {routines.map((Routine, idx) =>
-          <Stack.Screen key={idx} name={Routine.title} component={Routine} />
-        )}
+      <Stack.Navigator initialRouteName="Home" screenOptions={{
+        title: 'Rotinas HRT',
+        headerStyle: {
+          backgroundColor: collorPallete.white,
+        },
+        headerTintColor: collorPallete.black,
+        headerTitleStyle: {
+          fontWeight: 'normal',
+        },
+      }}>
+        <Stack.Screen name="Home" component={Home} options={{ title: 'Rotinas HRT' }} />
+        {
+          routines.map((Routine, idx) =>
+            <Stack.Screen key={idx} name={Routine.title} component={Routine} />
+          )
+        }
       </Stack.Navigator>
     </NavigationContainer>
 
