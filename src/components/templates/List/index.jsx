fc02833340e4ecd360e426b1b6d52de379bfa7ea
Luigi Minardi <luigiminardim@gmail.com>
2020-08-03 20:58:10 -0300

    Start styling lists.

--- src/components/templates/List/index.jsx
+++ src/components/templates/List/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Text } from 'react-native'
 import styled from 'styled-components';
+import Icon from 'react-native-vector-icons/Entypo'; Icon.loadFont()
 
-const List = styled.View`
+
+const List = styled.Text`
+  padding-left: 8px;
+  margin: 8px 0px;
 `
 
-const LItem = styled.Text`
+const SLitem = styled.Text`
+  font-size: 14px;
+  margin-bottom: 4px;
 `
+
+class LItem extends React.Component {
+  render = () =>
+    <SLitem>
+      <Icon name="dot-single" size={14} />{`${this.props.children}`}
+    </SLitem>
+}
+
+// function LItem({ children }) {
+//   return (
+//     <Text>{children ? '-' + children : console.log('Aqui:', children)}</Text>
+
+//   )
+// }
 
 export { List, LItem }
