11823f1ac8e84916e85f3a02677f63bd454ee896
Luigi Minardi <luigiminardim@gmail.com>
2020-08-03 11:42:02 -0300

    Added more collors to pallete.

--- src/assets/collorPallete.js
+++ src/assets/collorPallete.js
@@ -1,8 +1,10 @@
 export default {
   black: '#000000',
   blue: '#0000ff',
-  maximumBluePurple: '#b3b3ff',
+  blueHalfPastel: '#8080FF',
+  bluePastel: '#E6E6FF',
   red: '#ff0000',
-  lightPink: '#ffabab',
+  redHalfPastel: '#FF8080',
+  redPastel: '#FFE6E6',
   white: '#ffffff'
 }
