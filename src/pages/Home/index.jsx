31926c94a90f7b0ce3f4e6eb1ed77f8e1b5b1022
Luigi Minardi <luigiminardim@gmail.com>
2020-08-02 21:11:47 -0300

    Now, Home page divides it's content in clusters of routines.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,21 +9,31 @@ export default class Home extends React.Component {
 
   constructor(props) {
     super(props)
+    this.clusters = this.clustirezeRoutines(routines)
+    this.categories = Object.keys(this.clusters)
     this.state = {
-      selectedIndex: 0
+      selectedCategory: 0,
     }
   }
 
-  buttons = ['Todas categorias', 'Emergência e Terapia Intensiva', 'Infectologia', 'Hematologia, Reumatologia e Nefrologia', 'Pneumologia', 'Lactentes', 'Cardiologia', 'Neurologia', 'Endocrinologia', 'Gastroenterologia', 'Neonatologia', 'Pediatria geral e puericultura']
+  clustirezeRoutines = (routines) =>
+    routines.reduce((clusters, Routine) => {
+      if (!clusters[Routine.category]) {
+        clusters[Routine.category] = []
+      }
+      clusters['Todas categorias'].push(Routine)
+      clusters[Routine.category].push(Routine)
+      return clusters
+    }, { 'Todas categorias': [] })
 
   render = () =>
     <View>
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
         <ButtonGroup
-          onPress={(selectedIndex) => this.setState({ selectedIndex })}
-          selectedIndex={this.state.selectedIndex}
-          buttons={this.buttons}
-          containerStyle={{ backgroundColor: 'transparent', borderWidth: 0, margin:0 }}
+          onPress={(selectedCategory) => this.setState({ selectedCategory })}
+          selectedIndex={this.state.selectedCategory}
+          buttons={this.categories}
+          containerStyle={{ backgroundColor: 'transparent', borderWidth: 0, margin: 0 }}
           innerBorderStyle={{ width: 0 }}
           buttonContainerStyle={{ marginRight: 4 }}
           buttonStyle={{ minWidth: 70, maxWidth: 170, padding: 8, borderRadius: 4 }}
@@ -32,6 +42,8 @@ export default class Home extends React.Component {
           selectedButtonStyle={{ backgroundColor: 'lightgray' }}
         />
       </ScrollView>
-      {routines.map((Routine, idx) => <RoutineCard key={idx} Routine={Routine} navigation={this.props.navigation} />)}
+      {this.clusters[this.categories[this.state.selectedCategory]].map((Routine, idx) =>
+        <RoutineCard key={idx} Routine={Routine} navigation={this.props.navigation} />)
+      }
     </View>
 }
