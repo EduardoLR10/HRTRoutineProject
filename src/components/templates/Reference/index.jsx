f5ed07f4e8a7356b3c3656618191b32e6bd4879a
Luigi Minardi <luigiminardim@gmail.com>
2020-08-06 18:25:38 -0300

    Styled references.

--- src/components/templates/Reference/index.jsx
+++ src/components/templates/Reference/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import P from '../Paragraph'
+import { Text } from 'react-native'
+import styles from './styles';
 
+/**
+ * @property {number} props.idx - Index of the reference.
+ * @property {string} props.body - Content of the the reference item.
+ */
 export default class Reference extends React.Component {
 
-  render = () => {
-    let body = this.props.body ? `${this.props.body};` : ""
-    return (
-      <P>{`${this.props.idx}. ${body}`}</P>
-    )
-  }
+  render = () =>
+    <Text style={styles.reference}>
+      <Text style={styles.index}>{this.props.idx}</Text>. {this.props.body || ""}
+    </Text>
 }
