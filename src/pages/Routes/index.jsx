268205dc1bd1836735c8c2d89781b8fcf2236395
Luigi Minardi <luigiminardim@gmail.com>
2020-08-06 16:55:24 -0300

    Routes styles file splited from index.

--- src/pages/Routes/index.jsx
+++ src/pages/Routes/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Text from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, HeaderBackground } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
+import { screenOptions } from './styles'
 import Home from '../Home'
 import routines from '../../routines'
-import collorPallete from '../../assets/collorPallete'
-import { Animated } from 'react-native';
 
 const Stack = createStackNavigator()
 
@@ -13,23 +11,12 @@ export default class Routes extends React.Component {
 
   render = () =>
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home"
-        headerMode='float'
-        screenOptions={{
-          headerTintColor: collorPallete.red,
-          headerTitleAlign: 'center',
-          cardStyle: {
-            backgroundColor: collorPallete.white
-          }
-        }}
-        
-      >
+      <Stack.Navigator initialRouteName="Home" headerMode='float' screenOptions={screenOptions}>
         <Stack.Screen name="Home" component={Home} options={{ title: 'Rotinas HRT' }} />
         {routines.map((Routine, idx) =>
           <Stack.Screen key={idx} name={Routine.title} component={Routine} />
         )}
       </Stack.Navigator>
     </NavigationContainer>
-
-
+    
 }
