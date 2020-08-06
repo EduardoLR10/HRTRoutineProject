517810fb19a34d3c0a0016cfbfe9d244820521f9
Luigi Minardi <luigiminardim@gmail.com>
2020-08-06 18:13:56 -0300

    Take comments out.

--- src/components/templates/Reference/index.jsx
+++ src/components/templates/Reference/index.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import P from '../Paragraph'
 
-/**
- *  @property {number} idx
- *  @property {?string} authors
- *  @property {?string} title
- *  @property {?string} document
- *  @property {?string} publisher
- *  @property {?string} local
- *  @property {?string} year
- *  @property {?string} link
- */
 export default class Reference extends React.Component {
 
   render = () => {
