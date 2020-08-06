f10c21483d57eb71191e3aa7210203da9707bf75
Luigi Minardi <luigiminardim@gmail.com>
2020-08-06 18:13:43 -0300

    paragraphs styled.

---
+++ src/components/templates/Paragraph/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+import '../../../assets/collorPallete'
+import collorPallete from '../../../assets/collorPallete'
+
+export default StyleSheet.create({
+  paragraph: {
+    marginBottom: 7,
+    fontSize: 14, fontWeight: 'normal' ,color: collorPallete.black, 
+  },
+})
