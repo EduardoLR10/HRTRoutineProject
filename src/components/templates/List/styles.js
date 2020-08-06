a93e5790166a48868b0b5cf12e60c710299c8398
Luigi Minardi <luigiminardim@gmail.com>
2020-08-06 17:59:41 -0300

    Separeted styles in List component.

---
+++ src/components/templates/List/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+import collorPallete from '../../../assets/collorPallete'
+
+export default StyleSheet.create({
+  list: {
+    marginBottom: 7,
+    paddingLeft: 8,
+  },
+  litem: {
+    marginBottom: 7,
+    fontSize: 14, color: collorPallete.black,
+  },
+})
