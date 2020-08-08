79131071d8a7aba3dfc5a88d586198fa467a3ee4
Luigi Minardi <luigiminardim@gmail.com>
2020-08-08 20:47:44 -0300

    Figure remade to have modal.

--- src/components/templates/Figure/index.jsx
+++ src/components/templates/Figure/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Image, ScrollView, Text, View, Dimensions } from 'react-native'
-import styles from './styles'
+import { Image, ScrollView, Text, View, Dimensions, TouchableOpacity } from 'react-native'
+import styles, { imageStyles } from './styles'
 import ImageModal from 'react-native-image-modal';
 
 const { width, height } = Dimensions.get('window')
@@ -8,18 +8,9 @@ const { width, height } = Dimensions.get('window')
 export default class Figure extends React.Component {
 
   render = () =>
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', borderWidth: 2}}>
-      <ImageModal source={this.props.source} resizeMode='contain'
-        imageBackgroundColor={'white'} style={{ maxWidth: width, maxHeight: width, justifyContent: "center" }} />
-    </View>
-
-  render2 = () =>
     <View style={styles.container}>
       <Text style={styles.topLabel}>{this.props.top_label}</Text>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        <Image source={this.props.source} />
-      </ScrollView>
+      <ImageModal {...imageStyles} source={this.props.source} />
       <Text style={styles.bottomLabel}>{this.props.bottom_label}</Text>
     </View>
-
 }
