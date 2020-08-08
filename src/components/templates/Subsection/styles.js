2923eb121e4b60fde46eccb310a4085a3d394ee2
Luigi Minardi <luigiminardim@gmail.com>
2020-08-07 23:04:54 -0300

    Subsection styled.

---
+++ src/components/templates/Subsection/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+import collorPallete from '../../../assets/collorPallete'
+
+const containerStyle = (props, state) => ({
+  style: {
+    padding: 4,
+  },
+  titleStyle: {
+    padding: 8,
+    fontSize: 16, fontWeight: 'normal',
+    color: state.expanded ? collorPallete.red : collorPallete.blue
+  },
+  titleNumberOfLines: 0, // Don't define a limitit to title number of lines.
+})
+
+export { containerStyle }
