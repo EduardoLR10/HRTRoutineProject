2f944a738d852f7424f236170ad5928dc6c60c30
Luigi Minardi <luigiminardim@gmail.com>
2020-08-08 01:51:27 -0300

    Use react-native-image-modal

--- src/components/templates/Figure/index.jsx
+++ src/components/templates/Figure/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Image, ScrollView, Text, View } from 'react-native'
+import { Image, ScrollView, Text, View, Dimensions } from 'react-native'
 import styles from './styles'
+import ImageModal from 'react-native-image-modal';
+
+const { width, height } = Dimensions.get('window')
 
 export default class Figure extends React.Component {
 
   render = () =>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', borderWidth: 2}}>
+      <ImageModal source={this.props.source} resizeMode='contain'
+        imageBackgroundColor={'white'} style={{ maxWidth: width, maxHeight: width, justifyContent: "center" }} />
+    </View>
+
+  render2 = () =>
     <View style={styles.container}>
       <Text style={styles.topLabel}>{this.props.top_label}</Text>
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
