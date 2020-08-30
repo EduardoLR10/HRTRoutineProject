fb3a3e43df7c5bf85c0d98da970f05d6569cf532
Luigi Minardi <luigiminardim@gmail.com>
2020-08-30 15:04:44 -0300

    Added HeaderTitle sytles.js.

---
+++ src/components/HeaderTitle/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native'
+import collorPallete from '../../assets/collorPallete'
+
+export default StyleSheet.create({
+  container: {
+    fontSize: 20,
+    color: collorPallete.red
+  }
+})
