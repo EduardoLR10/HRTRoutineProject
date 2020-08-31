dcc8a9493d28c9b9c9490c16e13ac6341eb3f02b
Eduardo Lemos <dudulr10@gmail.com>
2020-08-31 16:20:23 -0300

    Preceptors data added

--- src/preceptors/index.js
+++ src/preceptors/index.js
@@ -1,9 +1,44 @@
-import imgLuigiMinardi from './images/LuigiMinardi.png'
-import imgEduardoLemos from './images/EduardoLemos.jpg'
-import imgMissLemos from './images/MissLemos.jpg'
+import AnaBeatriz from './images/AnaBeatriz.jpg'
+import AnaLuiza from './images/AnaLuiza.jpg'
+import Barbara from './images/Barbara.jpg'
+import Carlos from './images/Carlos.jpg'
+import Cristianne from './images/Cristianne.jpg'
+// import Dania from './images/Dania.jpg'
+import Danielle from './images/Danielle.jpg'
+import Fabricio from './images/Fabricio.jpg'
+import Fernando from './images/Fernando.jpg'
+import Francisco from './images/Francisco.jpg'
+import Iuri from './images/Iuri.jpg'
+import Jose from './images/Jose.jpg'
+import Luciany from './images/Luciany.jpg'
+import Luiz from './images/Luiz.jpg'
+import Manuela from './images/Manuela.jpg'
+import Marco from './images/Marco.jpg'
+import Rafael from './images/Rafael.jpg'
+import Renato from './images/Renato.jpg'
+import Roberta from './images/Roberta.jpg'
+import Sergio from './images/Sergio.jpg'
+
 
 export default [
-  { name: 'Luigi Minardi Ferreira Maia', image: imgLuigiMinardi, link: 'https://www.google.com/' },
-  { name: 'Eduardo Lemos Rocha', image: imgEduardoLemos, link: 'https://www.google.com/' },
-  { name: 'Dânia Lemos Dionízio', image: imgMissLemos, link: 'https://www.google.com/' },
+  { name: 'Ana Beatriz Schmitt Silva', image: AnaBeatriz, link: 'http://lattes.cnpq.br/8415928886394355' },
+  { name: 'Ana Luiza Melo dos Santos', image: AnaLuiza, link: 'http://lattes.cnpq.br/1989010115232505' },
+  { name: 'Bárbara Rios Vellasco de Amorim Vieira', image: Barbara, link: 'http://lattes.cnpq.br/7361011598717512' },
+  { name: 'Carlos Henrique Roriz da Rocha', image: Carlos, link: 'http://lattes.cnpq.br/5536788152769468' },
+  { name: 'Cristianne Teixeira Duarte Turra', image: Cristianne, link: 'http://lattes.cnpq.br/8208881125379372' },
+  // { name: 'Dania Lemos Dionízio', image: Dania, link: 'http://lattes.cnpq.br/9608663112029681' },
+  { name: 'Danielle Ribeiro Borges Noleto', image: Danielle, link: 'http://lattes.cnpq.br/3963486440074480' },
+  { name: 'Fabrício Pereira Madureira', image: Fabricio, link: 'http://lattes.cnpq.br/0778999649543914' },
+  { name: 'Fernando de Velasco Lino', image: Fernando, link: 'http://lattes.cnpq.br/6947266565311980' },
+  { name: 'Francisco Rufino Rosa Neto', image: Francisco, link: 'http://lattes.cnpq.br/0778999649543914' },
+  { name: 'Iúri Leão de Almeida', image: Iuri, link: 'http://lattes.cnpq.br/3677417132227767' },
+  { name: 'José Moreira Kffuri', image: Jose, link: 'http://lattes.cnpq.br/4163983800172589' },
+  { name: 'Luciany Almeida de Carvalho', image: Luciany, link: 'http://lattes.cnpq.br/8837363484026778' },
+  { name: 'Luiz Antônio de Almeida e Silva', image: Luiz, link: 'http://lattes.cnpq.br/0008443695134384' },
+  { name: 'Manuela de Oliveira Fragomeni', image: Manuela, link: 'http://lattes.cnpq.br/5590230320617326' },
+  { name: 'Marco Antônio Alves Cunha', image: Marco, link: 'http://lattes.cnpq.br/6523428843592229' },
+  { name: 'Rafael Belotti Fialho', image: Rafael, link: 'http://lattes.cnpq.br/4226810390608687' },
+  { name: 'Renato Resende Mundim', image: Renato, link: 'http://lattes.cnpq.br/6191682401941620' },
+  { name: 'Roberta Kelly Menezes Maciel Falleiros', image: Roberta, link: 'http://lattes.cnpq.br/6555325272608639' },
+  { name: 'Sérgio Carneiro de Mendonça', image: Sergio, link: 'http://lattes.cnpq.br/4003294033710189' },
 ]
