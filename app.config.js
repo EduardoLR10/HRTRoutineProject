c0ba21bd7571d04c3ddd7c10b347b0df6c55284b
Luigi Minardi <luigiminardim@gmail.com>
2020-09-03 23:25:32 -0300

    Optimized images.

--- app.config.js
+++ app.config.js
@@ -1,11 +1,9 @@
 export default {
   name: "Rotinas Pediatria HRT",
   slug: "rotinas-pediatria-hrt",
-  icon:
-    "./src/assets/images/logo.png",
+  icon: "./src/assets/images/logo.png",
   splash: {
-    image:
-      "./src/assets/images/logo.png",
+    image: "./src/assets/images/logo.png",
     resizeMode: "contain",
     backgroundColor: "#ffffff",
   },
