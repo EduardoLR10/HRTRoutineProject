7bd941b772ad7b1c3013b46526744986fc509998
Luigi Minardi <luigiminardim@gmail.com>
2020-09-04 00:53:26 -0300

    Minnor fixes.

--- app.config.js
+++ app.config.js
@@ -1,5 +1,7 @@
 export default {
+  version: "0.0.1",
   name: "Rotinas Pediatria HRT",
+  description: "Rotinas hospitalares de pediatria na palma de sua mão.",
   slug: "rotinas-pediatria-hrt",
   icon: "./src/assets/images/logo.png",
   splash: {
