95c266630a85c737a2e644ccc8b170747589743b
Luigi Minardi <luigiminardim@gmail.com>
2020-09-05 15:12:42 -0300

    Starting theme project.

--- src/components/HeaderTitle/styles.js
+++ src/components/HeaderTitle/styles.js
@@ -3,10 +3,7 @@ import collorPallete from '../../assets/collorPallete'
 
 export default StyleSheet.create({
   container: title => ({
-    fontSize: title.length > 40 ? 14
-      : title.length > 35 ? 16
-        : title.length > 30 ? 18
-          : 20,
+    fontSize: title.length > 40 ? 14 : title.length > 30 ? 15 : title.length > 20 ? 16 : 17,
     color: collorPallete.red
   })
 })
