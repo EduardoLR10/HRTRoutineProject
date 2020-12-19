10ec86f484fb17ee31bed7bd3af3e8bf8a4e9f9a
Luigi Minardi <luigiminardim@gmail.com>
2020-12-19 18:05:59 -0300

    Added authors.

--- src/App/contexts/AuthorsContext/authors.ts
+++ src/App/contexts/AuthorsContext/authors.ts
@@ -1,5 +1,25 @@
 import Author from '../../../models/Author'
 import avatarLuigiMinardi from './images/LuigiMinardi.png'
+import AnaBeatriz from './images/AnaBeatriz.jpg'
+import AnaLuiza from './images/AnaLuiza.jpg'
+import Barbara from './images/Barbara.jpg'
+import Carlos from './images/Carlos.jpg'
+import Cristianne from './images/Cristianne.jpg'
+import Dania from './images/Dania.jpg'
+import Danielle from './images/Danielle.jpg'
+import Fabricio from './images/Fabricio.jpg'
+import Fernando from './images/Fernando.jpg'
+import Francisco from './images/Francisco.jpg'
+import Iuri from './images/Iuri.jpg'
+import Jose from './images/Jose.jpg'
+import Luciany from './images/Luciany.jpg'
+import Luiz from './images/Luiz.jpg'
+import Manuela from './images/Manuela.jpg'
+import Marco from './images/Marco.jpg'
+import Rafael from './images/Rafael.jpg'
+import Renato from './images/Renato.jpg'
+import Roberta from './images/Roberta.jpg'
+import Sergio from './images/Sergio.jpg'
 
 export default [
   new Author(
@@ -9,27 +29,123 @@ export default [
     'https://www.google.com.br/'
   ),
   new Author(
-    'test_author_1',
-    'Autor de Testes 1',
-    avatarLuigiMinardi,
-    'https://www.google.com.br/'
+    'ana-beatriz',
+    'Ana Beatriz Schmitt Silva',
+    AnaBeatriz,
+    'http://lattes.cnpq.br/8415928886394355'
   ),
   new Author(
-    'test_author_2',
-    'Autor de Testes 2',
-    avatarLuigiMinardi,
-    'https://www.google.com.br/'
+    'ana-luiza',
+    'Ana Luiza Melo dos Santos',
+    AnaLuiza,
+    'http://lattes.cnpq.br/1989010115232505'
   ),
   new Author(
-    'test_author_3',
-    'Autor de Testes 3',
-    avatarLuigiMinardi,
-    'https://www.google.com.br/'
+    'barbara',
+    'Bárbara Rios Vellasco de Amorim Vieira',
+    Barbara,
+    'http://lattes.cnpq.br/7361011598717512'
   ),
   new Author(
-    'test_author_4',
-    'Autor de Testes 4',
-    avatarLuigiMinardi,
-    'https://www.google.com.br/'
+    'carlos',
+    'Carlos Henrique Roriz da Rocha',
+    Carlos,
+    'http://lattes.cnpq.br/5536788152769468'
+  ),
+  new Author(
+    'cristianne',
+    'Cristianne Teixeira Duarte Turra',
+    Cristianne,
+    'http://lattes.cnpq.br/8208881125379372'
+  ),
+  new Author(
+    'dania',
+    'Dania Lemos Dionízio',
+    Dania,
+    'http://lattes.cnpq.br/9608663112029681'
+  ),
+  new Author(
+    'danielle',
+    'Danielle Ribeiro Borges Noleto',
+    Danielle,
+    'http://lattes.cnpq.br/3963486440074480'
+  ),
+  new Author(
+    'fabricio',
+    'Fabrício Pereira Madureira',
+    Fabricio,
+    'http://lattes.cnpq.br/0778999649543914'
+  ),
+  new Author(
+    'fernando',
+    'Fernando de Velasco Lino',
+    Fernando,
+    'http://lattes.cnpq.br/6947266565311980'
+  ),
+  new Author(
+    'francisco',
+    'Francisco Rufino Rosa Neto',
+    Francisco,
+    'http://lattes.cnpq.br/0778999649543914'
+  ),
+  new Author(
+    'iuri',
+    'Iúri Leão de Almeida',
+    Iuri,
+    'http://lattes.cnpq.br/3677417132227767'
+  ),
+  new Author(
+    'jose',
+    'José Moreira Kffuri',
+    Jose,
+    'http://lattes.cnpq.br/4163983800172589'
+  ),
+  new Author(
+    'luciany',
+    'Luciany Almeida de Carvalho',
+    Luciany,
+    'http://lattes.cnpq.br/8837363484026778'
+  ),
+  new Author(
+    'luiz',
+    'Antônio de Almeida e Silva',
+    Luiz,
+    'http://lattes.cnpq.br/0008443695134384'
+  ),
+  new Author(
+    'manuela',
+    'Manuela de Oliveira Fragomeni',
+    Manuela,
+    'http://lattes.cnpq.br/5590230320617326'
+  ),
+  new Author(
+    'marco',
+    'Marco Antônio Alves Cunha',
+    Marco,
+    'http://lattes.cnpq.br/6523428843592229'
+  ),
+  new Author(
+    'rafael',
+    'Rafael Belotti Fialho',
+    Rafael,
+    'http://lattes.cnpq.br/4226810390608687'
+  ),
+  new Author(
+    'renato',
+    'Renato Resende Mundim',
+    Renato,
+    'http://lattes.cnpq.br/6191682401941620'
+  ),
+  new Author(
+    'roberta',
+    'Roberta Kelly Menezes Maciel Falleiros',
+    Roberta,
+    'http://lattes.cnpq.br/6555325272608639'
+  ),
+  new Author(
+    'sergio',
+    'Sérgio Carneiro de Mendonça',
+    Sergio,
+    'http://lattes.cnpq.br/4003294033710189'
   )
 ]
