4947b1d1413a2bc133187cbab97828aedaaab05f
Luigi Minardi <luigiminardim@gmail.com>
2021-01-14 09:29:46 -0300

    Added degradee to color bar.

--- src/App/shared/CategoryItem.tsx
+++ src/App/shared/CategoryItem.tsx
@@ -1,17 +1,18 @@
+import { HeaderBackground } from '@react-navigation/stack'
+import { LinearGradient } from 'expo-linear-gradient'
 import React from 'react'
 import { TouchableRipple } from 'react-native-paper'
 import styled, { useTheme } from 'styled-components/native'
 import Category from '../../models/Category'
 import { Subtitle1 } from './../shared/typography'
 
-const Container = styled(TouchableRipple)<{ hue: number; selected: boolean }>`
+const Background = styled(LinearGradient)<{ selected: boolean }>`
   width: 152px;
   height: 76px;
   border-radius: 8px;
-  padding: 8px;
-  background-color: hsl(${props => props.hue}, 100%, 35%);
   box-shadow: ${props => props.theme.elevation[1]};
   opacity: ${props => (props.selected ? 1 : 0.25)};
+  overflow: hidden;
 `
 export interface CategoryItemProps {
   category: Category
@@ -26,8 +27,18 @@ export default function CategoryItem({
 }: CategoryItemProps): JSX.Element {
   const theme = useTheme()
   return (
-    <Container hue={category.hueTheme} onPress={onPress} selected={selected}>
-      <Subtitle1 color={theme.color.onPrimary}>{category.name}</Subtitle1>
-    </Container>
+    <Background
+      selected={selected}
+      colors={[
+        `hsl(${category.hueTheme}, 100%, 35%)`,
+        `hsl(${category.hueTheme}, 100%, 50%)`
+      ]}
+      start={[0, 0]}
+      end={[1, 0]}
+    >
+      <TouchableRipple style={{ flexGrow: 1, padding: 8 }} onPress={onPress}>
+        <Subtitle1 color={theme.color.onPrimary}>{category.name}</Subtitle1>
+      </TouchableRipple>
+    </Background>
   )
 }
