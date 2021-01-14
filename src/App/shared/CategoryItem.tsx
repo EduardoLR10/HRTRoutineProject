4a7f0c8f3671d2ff55ae5a30e4527a20a9049fc9
Luigi Minardi <luigiminardim@gmail.com>
2021-01-14 09:56:43 -0300

    Increse focused elevation on Category card.

--- src/App/shared/CategoryItem.tsx
+++ src/App/shared/CategoryItem.tsx
@@ -1,4 +1,3 @@
-import { HeaderBackground } from '@react-navigation/stack'
 import { LinearGradient } from 'expo-linear-gradient'
 import React from 'react'
 import { TouchableRipple } from 'react-native-paper'
@@ -6,17 +5,21 @@ import styled, { useTheme } from 'styled-components/native'
 import Category from '../../models/Category'
 import { Subtitle1 } from './../shared/typography'
 
-const Background = styled(LinearGradient)<{ selected: boolean }>`
+const Background = styled(LinearGradient)<{
+  selected: boolean
+  focus: boolean
+}>`
   width: 152px;
   height: 76px;
   border-radius: 8px;
-  box-shadow: ${props => props.theme.elevation[1]};
+  box-shadow: ${props =>
+    props.focus ? props.theme.elevation[8] : props.theme.elevation[1]};
   opacity: ${props => (props.selected ? 1 : 0.25)};
   overflow: hidden;
 `
 export interface CategoryItemProps {
   category: Category
-  selected: boolean
+  selected: Category
   onPress?: () => void
 }
 
@@ -26,9 +29,11 @@ export default function CategoryItem({
   selected
 }: CategoryItemProps): JSX.Element {
   const theme = useTheme()
+
   return (
     <Background
-      selected={selected}
+      selected={selected === null || selected === category}
+      focus={selected === category}
       colors={[
         `hsl(${category.hueTheme}, 100%, 35%)`,
         `hsl(${category.hueTheme}, 100%, 50%)`
