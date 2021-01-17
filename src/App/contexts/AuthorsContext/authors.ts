3b47226189efe196246cd89382fc3015f5e9e591
Eduardo Lemos Rocha <dudulr10@gmail.com>
2021-01-17 15:25:32 -0300

    Update authors.ts

--- src/App/contexts/AuthorsContext/authors.ts
+++ src/App/contexts/AuthorsContext/authors.ts
@@ -24,127 +24,127 @@ import Sergio from './images/Sergio.jpg'
 export default [
   new Author(
     'ana-beatriz',
-    'Ana Beatriz Schmitt Silva',
+    'Ana Beatriz S. Silva',
     AnaBeatriz,
     'http://lattes.cnpq.br/8415928886394355'
   ),
   new Author(
     'ana-luiza',
-    'Ana Luiza Melo dos Santos',
+    'Ana Luiza M. dos Santos',
     AnaLuiza,
     'http://lattes.cnpq.br/1989010115232505'
   ),
   new Author(
     'barbara',
-    'Bárbara Rios Vellasco de Amorim Vieira',
+    'Bárbara R. V. de A. Vieira',
     Barbara,
     'http://lattes.cnpq.br/7361011598717512'
   ),
   new Author(
     'carlos',
-    'Carlos Henrique Roriz da Rocha',
+    'Carlos Henrique R. da Rocha',
     Carlos,
     'http://lattes.cnpq.br/5536788152769468'
   ),
   new Author(
     'cristianne',
-    'Cristianne Teixeira Duarte Turra',
+    'Cristianne T. D. Turra',
     Cristianne,
     'http://lattes.cnpq.br/8208881125379372'
   ),
   new Author(
     'dania',
-    'Dania Lemos Dionízio',
+    'Dania L. Dionízio',
     Dania,
     'http://lattes.cnpq.br/9608663112029681'
   ),
   new Author(
     'danielle',
-    'Danielle Ribeiro Borges Noleto',
+    'Danielle R. B. Noleto',
     Danielle,
     'http://lattes.cnpq.br/3963486440074480'
   ),
   new Author(
     'fabricio',
-    'Fabrício Pereira Madureira',
+    'Fabrício P. Madureira',
     Fabricio,
     'http://lattes.cnpq.br/0778999649543914'
   ),
   new Author(
     'fernando',
-    'Fernando de Velasco Lino',
+    'Fernando de V. Lino',
     Fernando,
     'http://lattes.cnpq.br/6947266565311980'
   ),
   new Author(
     'francisco',
-    'Francisco Rufino Rosa Neto',
+    'Francisco R. R. Neto',
     Francisco,
     'http://lattes.cnpq.br/0778999649543914'
   ),
   new Author(
     'iuri',
-    'Iúri Leão de Almeida',
+    'Iúri L. de Almeida',
     Iuri,
     'http://lattes.cnpq.br/3677417132227767'
   ),
   new Author(
     'jose',
-    'José Moreira Kffuri',
+    'José M. Kffuri',
     Jose,
     'http://lattes.cnpq.br/4163983800172589'
   ),
   new Author(
     'luciany',
-    'Luciany Almeida de Carvalho',
+    'Luciany A. de Carvalho',
     Luciany,
     'http://lattes.cnpq.br/8837363484026778'
   ),
   new Author(
     'luiz',
-    'Antônio de Almeida e Silva',
+    'Antônio A. e Silva',
     Luiz,
     'http://lattes.cnpq.br/0008443695134384'
   ),
   new Author(
     'manuela',
-    'Manuela de Oliveira Fragomeni',
+    'Manuela de O. Fragomeni',
     Manuela,
     'http://lattes.cnpq.br/5590230320617326'
   ),
   new Author(
     'marco',
-    'Marco Antônio Alves Cunha',
+    'Marco Antônio A. Cunha',
     Marco,
     'http://lattes.cnpq.br/6523428843592229'
   ),
   new Author(
     'maria',
-    'Maria Aparecida Soares Moreira Machado',
+    'Maria Aparecida S. M. Machado',
     Maria,
     'http://lattes.cnpq.br/8591755904908251'
   ),
   new Author(
     'rafael',
-    'Rafael Belotti Fialho',
+    'Rafael B. Fialho',
     Rafael,
     'http://lattes.cnpq.br/4226810390608687'
   ),
   new Author(
     'renato',
-    'Renato Resende Mundim',
+    'Renato R. Mundim',
     Renato,
     'http://lattes.cnpq.br/6191682401941620'
   ),
   new Author(
     'roberta',
-    'Roberta Kelly Menezes Maciel Falleiros',
+    'Roberta Kelly M. M. Falleiros',
     Roberta,
     'http://lattes.cnpq.br/6555325272608639'
   ),
   new Author(
     'sergio',
-    'Sérgio Carneiro de Mendonça',
+    'Sérgio C. de Mendonça',
     Sergio,
     'http://lattes.cnpq.br/4003294033710189'
   )
