755e38114bcef27623704de042b08c02b8c8f2b4
EduardoLR10 <dudulr10@gmail.com>
2023-06-05 15:50:37 -0300

    Fix Convulsao Febril routine

--- src/App/contexts/RoutinesContext/routines/ConvulsaoFebril/index.tsx
+++ src/App/contexts/RoutinesContext/routines/ConvulsaoFebril/index.tsx
@@ -26,31 +26,33 @@ export default function ConvulsaoFebril(): JSX.Element {
       tags={['febre', 'crise', 'convulsiva']}
     >
       <Section title="Definições">
-        <P>
-          Convulsão acompanhada de febre (temperatura ≥ 38°C, por qualquer
-          método), sem evidência de infecção do sistema nervoso central, em
-          lactentes e crianças entre 3 meses e 5 anos de idade.
-        </P>
-        <P>
-          Causa mais comum de crises epilépticas na infância, de curso benigno e
-          autolimitado.
-        </P>
-        <P>
-          Geralmente ocorre nas primeiras 24 horas da doença e 21% na primeira
-          hora após início da febre. Crises que ocorrem 3 dias ou mais após o
-          início do quadro, dificilmente serão convulsões febris.
-        </P>
-        <P>
-          Nos primeiros 2 anos de vida, cerca de 2/3 dos estados de mal
-          epiléptico (EME) são febris. A febre geralmente está mais alta (cerca
-          de 39°C) no momento da crise, que é o primeiro sinal de febre em 25-
-          50% dos casos.
-        </P>
-        <P>
-          Fatores de risco: elevação da temperatura, história familiar positiva
-          em parentes de 1º grau, internação &gt; 28 dias no período neonatal,
-          atraso do desenvolvimento neurológico, frequentar creche.
-        </P>
+        <List>
+          <LItem>
+            Convulsão acompanhada de febre (temperatura ≥ 38°C, por qualquer
+            método), sem evidência de infecção do sistema nervoso central, em
+            lactentes e crianças entre 3 meses e 5 anos de idade.
+          </LItem>
+          <LItem>
+            Causa mais comum de crises epilépticas na infância, de curso benigno e
+            autolimitado.
+          </LItem>
+          <LItem>
+            Geralmente ocorre nas primeiras 24 horas da doença e 21% na primeira
+            hora após início da febre. Crises que ocorrem 3 dias ou mais após o
+            início do quadro, dificilmente serão convulsões febris.
+          </LItem>
+          <LItem>
+            Nos primeiros 2 anos de vida, cerca de 2/3 dos estados de mal
+            epiléptico (EME) são febris. A febre geralmente está mais alta (cerca
+            de 39°C) no momento da crise, que é o primeiro sinal de febre em 25-
+            50% dos casos.
+          </LItem>
+          <LItem>
+            Fatores de risco: elevação da temperatura, história familiar positiva
+            em parentes de 1º grau, internação &gt; 28 dias no período neonatal,
+            atraso do desenvolvimento neurológico, frequentar creche.
+          </LItem>
+        </List>
       </Section>
       <Section title="Diagnóstico Clínico">
         <Table
@@ -195,7 +197,7 @@ export default function ConvulsaoFebril(): JSX.Element {
         <P>
           Risco de epilepsia: cerca de 3% dos pacientes com convulsões febris
           desenvolverá epilepsia, sendo o risco mais elevado em crianças com
-          crises complexas. Os principais fatores de risco são epilepsia
+          crises complexas. Os principais fatores de risco são: epilepsia
           genética / idiopática em parente de 1º grau, atraso prévio do
           desenvolvimento neurológico, crises prolongadas / EME febril,
           paralisia cerebral, APGAR baixo e crises focais / recorrentes.
